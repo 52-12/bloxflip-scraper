7a0aed10afb01461a99c29533f285a31213e9003
5212 <3upmanl0128@gmail.com>
2023-08-13 15:49:45 -0400

    it no work on real site time to debug

--- test.ts
+++ test.ts
@@ -7,16 +7,19 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 chromium.use(StealthPlugin())
 chromium.launch({ headless: false }).then(async browser => {
     const page = await browser.newPage()
-    // await page.goto('http://127.0.0.1:3000/webpage/Adamslayz_you.html')
     // await page.goto('http://127.0.0.1:3000/webpage/6unfiree.html')
+    // await page.goto('http://127.0.0.1:3000/webpage/Adamslayz_you.html')
+    // await page.goto('http://127.0.0.1:3000/webpage/NoobyNolax.html')
     await page.goto('https://bloxflip.com/')
-
+    
     const parent = page.getByText('It’s about to rain!').locator('xpath=..');
-
+    
     await parent.waitFor({ timeout: 0 });
-
+    
     const paragraph = parent.getByRole("paragraph").filter({ hasText: 'participants' });
-    console.info(await paragraph.innerHTML())
+    
+    console.info(await page.content())
+    // console.info(await paragraph.innerHTML())
 
     let amountOfRobux = await paragraph.evaluate(p => p.childNodes[0].textContent);
     let participants = await paragraph.evaluate(p => p.childNodes[2].textContent);
@@ -51,22 +54,21 @@ chromium.launch({ headless: false }).then(async browser => {
         host = 'It\'s broken! :('
     }
 
-    // console.log('after regex:', {"amountOfRobux": amountOfRobux, "participants": participants, "host": host})
+    
+    console.log('after regex:', {"amountOfRobux": amountOfRobux, "participants": participants, "host": host})
+    // const webhookClient = new WebhookClient({ id: config.webhookId, token: config.webhookToken });
 
-    const webhookClient = new WebhookClient({ id: config.webhookId, token: config.webhookToken });
+    // const embed = new EmbedBuilder()
+    //     .setTitle(amountOfRobux + " Rain")
+    //     .setURL('https://bloxflip.com/')
+    //     .setColor(0x00FFFF)
+    //     .setTimestamp()
+    //     .setDescription(`Host: ${host}\nParticipants: ${participants}`)
 
-    const embed = new EmbedBuilder()
-        .setTitle(amountOfRobux + " Rain")
-        .setURL('https://bloxflip.com/')
-        .setColor(0x00FFFF)
-        .setTimestamp()
-        .setDescription(`Host: ${host}\nParticipants: ${participants}`)
-
-    webhookClient.send({
-        username: 'cool rain notifer bot',
-        embeds: [embed],
-    });
-
+    // webhookClient.send({
+    //     username: 'cool rain notifer bot',
+    //     embeds: [embed],
+    // });
 
     await browser.close()
 })
