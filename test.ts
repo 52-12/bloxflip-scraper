b22ebe2ab641b93003c21fca4f14faad7d1bc5cc
5212 <3upmanl0128@gmail.com>
2023-08-15 19:46:18 -0400

    change child nodes to innertext and regex

--- test.ts
+++ test.ts
@@ -10,6 +10,7 @@ chromium.launch({ headless: false }).then(async browser => {
     // await page.goto('http://127.0.0.1:3000/webpage/6unfiree.html')
     // await page.goto('http://127.0.0.1:3000/webpage/Adamslayz_you.html')
     // await page.goto('http://127.0.0.1:3000/webpage/NoobyNolax.html')
+    // await page.goto('http://127.0.0.1:3000/webpage/PFB1cg.html')
     await page.goto('https://bloxflip.com/')
     
     const parent = page.getByText('It’s about to rain!').locator('xpath=..');
@@ -18,57 +19,31 @@ chromium.launch({ headless: false }).then(async browser => {
     
     const paragraph = parent.getByRole("paragraph").filter({ hasText: 'participants' });
     
-    console.info(await page.content())
-    // console.info(await paragraph.innerHTML())
-
-    let amountOfRobux = await paragraph.evaluate(p => p.childNodes[0].textContent);
-    let participants = await paragraph.evaluate(p => p.childNodes[2].textContent);
-    let host = await paragraph.evaluate(p => p.childNodes[4].textContent);
-    console.log('before regex:', { "amountOfRobux": amountOfRobux, "participants": participants, "host": host })
-
-    // do regex cleanup
-    if (amountOfRobux) {
-        const match = amountOfRobux.match('\\b\\d[\\d,.]*\\b')
-        if (match) {
-            amountOfRobux = match[0]
-        }
-    } else {
-        amountOfRobux = 'It\'s broken! :('
-    }
-
-    if (participants) {
-        const match = participants.match('\\b\\d[\\d,.]*\\b')
-        if (match) {
-            participants = match[0]
-        }
-    } else {
-        participants = 'It\'s broken! :('
-    }
-
-    if (host) {
-        const match = host.match('by (.*)')
-        if (match) {
-            host = match[1]
-        }
-    } else {
-        host = 'It\'s broken! :('
-    }
+    // console.info(await page.content())
+    console.info(await paragraph.innerHTML())
 
     
-    console.log('after regex:', {"amountOfRobux": amountOfRobux, "participants": participants, "host": host})
-    // const webhookClient = new WebhookClient({ id: config.webhookId, token: config.webhookToken });
+    const numbersRegex = (await paragraph.innerText()).match(/\b\d[\d,.]*\b/g)
+    const amountOfRobux = numbersRegex ? numbersRegex[0] : "it's broken"
+    const participants = numbersRegex ? numbersRegex[1] : "it's broken"
 
-    // const embed = new EmbedBuilder()
-    //     .setTitle(amountOfRobux + " Rain")
-    //     .setURL('https://bloxflip.com/')
-    //     .setColor(0x00FFFF)
-    //     .setTimestamp()
-    //     .setDescription(`Host: ${host}\nParticipants: ${participants}`)
+    const hostRegex = (await paragraph.innerText()).match(/by (.*)/)
+    const host = hostRegex ? hostRegex[1] : "it's broken"
+    console.log({ "amountOfRobux": amountOfRobux, "participants": participants, "host": host[1] })
 
-    // webhookClient.send({
-    //     username: 'cool rain notifer bot',
-    //     embeds: [embed],
-    // });
+    const webhookClient = new WebhookClient({ id: config.webhookId, token: config.webhookToken });
+
+    const embed = new EmbedBuilder()
+        .setTitle(`${amountOfRobux} Rain`)
+        .setURL('https://bloxflip.com/')
+        .setColor(0x00FFFF)
+        .setTimestamp()
+        .setDescription(`Host: ${host}\nParticipants: ${participants} participants`)
+
+    webhookClient.send({
+        username: 'cool rain notifer bot',
+        embeds: [embed],
+    });
 
     await browser.close()
 })
