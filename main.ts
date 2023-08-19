db590ecfaf656b9cf770c3f3c4353a055372e25d
5212 <3upmanl0128@gmail.com>
2023-08-19 13:31:31 -0400

    Fixed edit multiple webhooks bug

--- main.ts
+++ main.ts
@@ -70,42 +70,51 @@ chromium.launch({ headless: false }).then(async browser => {
         new Promise((resolve, reject) => {
             
             const loop = () => {
-                setTimeout(async () => {
-                    let newRainInfo = {amountOfRobux: '', participants: '', host: ''}
+                if (runLoop.has(webhookMessage.id)) {
+                    setTimeout(async () => {
+                        let newRainInfo = {amountOfRobux: '', participants: '', host: ''}
 
 
-                    // 1. get's the page's inner text
-                    try {
-                        newRainInfo = await getParagraphState(paragraph)
-                    } catch (error) {
-                        if (error instanceof playwright.errors.TimeoutError) {
-                            if (!runLoop.has(webhookMessage.id)) {
-                                console.log(`Stopped editing message: ${webhookMessage.id}`)
+                        // 1. get's the page's inner text
+                        try {
+                            newRainInfo = await getParagraphState(paragraph)
+                        } catch (error) {
+                            if (error instanceof playwright.errors.TimeoutError) {
+                                console.log(`Stopped editing message (timeout): ${webhookMessage.id}`)
                                 return resolve(true)
+                            } else {
+                                throw error;
                             }
-                        } else {
-                            throw error;
                         }
-                    }
+
+                        if (!runLoop.has(webhookMessage.id)) {
+                            console.log(`Stopped editing message (this rain is not happening anymore, after we checked inner text): ${webhookMessage.id}`)
+                            return resolve(true)
+                        }
 
 
-                    // 2. checks if inner text participants is different then the one on discord
-                    if (newRainInfo.participants !== rainInfo.participants) {
+                        // 2. checks if inner text participants is different then the one on discord
+                        if (newRainInfo.participants !== rainInfo.participants) {
 
-                        // 3a. if it is different: edits the message on discord.
-                        rainInfo = newRainInfo;
-                        console.log('Updating participants...', { "amountOfRobux": rainInfo.amountOfRobux, "participants": rainInfo.participants, "host": rainInfo.host })
-                        await webhookClient.editMessage(webhookMessage.id, {
-                            embeds: [getEmbed(rainInfo.amountOfRobux, rainInfo.host, rainInfo.participants)],
-                        });
-                    } else {
+                            // 3a. if it is different: edits the message on discord.
+                            rainInfo = newRainInfo;
+                            console.log('Updating participants...', { "amountOfRobux": rainInfo.amountOfRobux, "participants": rainInfo.participants, "host": rainInfo.host })
+                            await webhookClient.editMessage(webhookMessage.id, {
+                                embeds: [getEmbed(rainInfo.amountOfRobux, rainInfo.host, rainInfo.participants)],
+                            });
+                        } else {
 
-                         // 3b. if it is the same: does not edit
-                        console.log(`Not editing message since it's the same: ${webhookMessage.id}`)
-                    }
+                            // 3b. if it is the same: does not edit
+                            console.log(`Not editing message since it's the same: ${webhookMessage.id}`)
+                        }
 
                     loop() // starts the loop all over again
-                }, 5000);
+                    }, 5000);
+
+                } else {
+                    console.log(`Stopped editing message (this rain is not happening anymore, after we sent a edit): ${webhookMessage.id}`)
+                    return resolve(true)
+                }
             };
             
             loop()
